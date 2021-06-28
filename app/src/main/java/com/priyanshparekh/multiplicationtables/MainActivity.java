e4d0f36e534f51b03f331cf84e7f0212e939a4fb
Priyansh Parekh <parekhpriyansh20@gmail.com>
2021-06-28 16:27:10 +0530

    Some Changes

--- app/src/main/java/com/priyanshparekh/multiplicationtables/MainActivity.java
+++ app/src/main/java/com/priyanshparekh/multiplicationtables/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
@@ -39,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
 
         textView2 = findViewById(R.id.textView2);
         adView = findViewById(R.id.bannerAd);
+
+        AdView adView1 = new AdView(this);
+        adView1.setAdSize(AdSize.BANNER);
+        adView1.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
 
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
